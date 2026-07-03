9684e9b8d83844815cdef6ee9a20fdfaabe1e977
Henry4002 <0001139164@senaimgaluno.com.br>
2026-07-03 09:43:30 -0300

    Adiciona script SQL atualizado

---
+++ database/scriptatualizado.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE IF NOT EXISTS tcc_gestao_produtos;
+USE tcc_gestao_produtos;
+
+CREATE TABLE perfil (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    idPerfil INT NOT NULL,
+    FOREIGN KEY (idPerfil) REFERENCES perfil(id)
+);
+
+CREATE TABLE status_produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao TEXT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    idStatus INT NOT NULL,
+    idUsuario INT NOT NULL,
+    FOREIGN KEY (idStatus) REFERENCES status_produto(id),
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE estoque (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_atualizacao DATETIME NOT NULL,
+    qtd_minima INT NOT NULL,
+    quantidade INT NOT NULL,
+    idProduto INT NOT NULL UNIQUE,
+    FOREIGN KEY (idProduto) REFERENCES produto(id)
+);
+
+CREATE TABLE receita (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    idProduto INT NOT NULL,
+    FOREIGN KEY (idProduto) REFERENCES produto(id)
+);
+
+CREATE TABLE versao_receita (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    numero_versao INT NOT NULL,
+    data_versao DATE NOT NULL,
+    descricao TEXT NOT NULL,
+    idReceita INT NOT NULL,
+    FOREIGN KEY (idReceita) REFERENCES receita(id)
+);
+
+CREATE TABLE teste (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_teste DATE NOT NULL,
+    resultado VARCHAR(200) NOT NULL,
+    idUsuario INT NOT NULL,
+    idVersao_receita INT NOT NULL,
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+    FOREIGN KEY (idVersao_receita) REFERENCES versao_receita(id)
+);
+
+CREATE TABLE historico (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    acao TEXT NOT NULL,
+    data_historico DATETIME NOT NULL,
+    idProduto INT NOT NULL,
+    idUsuario INT NOT NULL,
+    FOREIGN KEY (idProduto) REFERENCES produto(id),
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
+);
+
+
+
+
+
+
+
+
+
+
